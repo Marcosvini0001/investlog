609a337f9a512087e4d4a09357ce8ec049247f52
Marcos Vinicius <mvsenko7@gmail.com>
2026-05-16 21:20:08 -0300

    att

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -6,8 +6,16 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
     dialect: 'mysql',
     logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
